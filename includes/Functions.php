9f395f767d8db92a31dabb65fd897087bd4734ed
Aaron <aaron@ajexperience.com>
2019-02-28 13:01:39 +0100

    Better support for sites with 10k+ pages - ajax dropdowns work better.

--- includes/Functions.php
+++ includes/Functions.php
@@ -1,8 +1,7 @@
 <?php
 
 // turn on debug for localhost etc
-$whitelist = array('127.0.0.1', '::1', 'localhost', 'wealth-psychology.com', 'www.wealth-psychology.com');
-if (in_array($_SERVER['SERVER_NAME'], $whitelist)) {
+if (in_array($_SERVER['SERVER_NAME'], $GLOBALS['abj404_whitelist'])) {
     error_reporting(E_ALL);
     ini_set('display_errors', '1');
 }
