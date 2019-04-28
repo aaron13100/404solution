aaron13100/404solution

commit 2366c317c81a749e8918edb1566a59a9946ff345

FIX: Correct the 'requested URL can't be null' logging issue.

includes/objs/UserRequest.php
@@ -46,12 +46,14 @@ class ABJ_404_Solution_UserRequest implements JsonSerializable {
         
         // hanlde the case where '///?gf_page=upload' is returned as the request URI.
         $urlToParse = urldecode($_SERVER['REQUEST_URI']);
+        $containsHost = $f->strpos($urlToParse, "://");
         
-        if (!is_array(parse_url(esc_url($urlToParse)))) {
-            if ($f->substr($urlToParse, 0, 1) == "/") {
-                $urlToParse = home_url($wp->request) . $f->substr($urlToParse, 1);
-            }
+        if (($containsHost === false) || ($containsHost >= 7) || (!is_array(parse_url(esc_url($urlToParse))))) {
+            // we have something like //login.php and it needs to be http://host.com/login.php
+            $urlToParse = str_replace('//', '/', $urlToParse);
+            $urlToParse = get_site_url() . $urlToParse;
         }
+        
         $urlParts = parse_url(esc_url($urlToParse));
         if (!is_array($urlParts)) {
             $abj404logging->errorMessage('parse_url returned a non-array value. REQUEST_URI: "' . 

includes/DataAccess.php
@@ -367,8 +367,8 @@ class ABJ_404_Solution_DataAccess {
         $results['last_query'] = $wpdb->last_query;
 
         if ($wpdb->last_error != '') {
-            $abj404logging->errorMessage("Ugh. SQL insert error: " . esc_html($errorThisRun) . 
-                    ", Query: " . esc_html($queryThisRun));
+            $abj404logging->errorMessage("Ugh. SQL insert error: " . $errorThisRun . 
+                    ", Query: " . $queryThisRun);
         }
 
         return $results;
@@ -791,15 +791,12 @@ class ABJ_404_Solution_DataAccess {
     
     /** 
      * Log that a redirect was done. Insert into the logs table.
-     * @global type $wpdb
-     * @global type $abj404logging
      * @param type $requestedURL
      * @param type $action
      * @param type $matchReason
      * @param type $requestedURLDetail the exact URL that was requested, for cases when a regex URL was matched.
      */
     function logRedirectHit($requestedURL, $action, $matchReason, $requestedURLDetail = null) {
-        global $wpdb;
         $abj404logging = ABJ_404_Solution_Logging::getInstance();
         $abj404logic = new ABJ_404_Solution_PluginLogic();
         

includes/View.php
@@ -927,11 +927,11 @@ class ABJ_404_Solution_View {
 
             $editlink = "?page=" . ABJ404_PP . "&subpage=abj404_edit&id=" . $row['id'];
             $logslink = "?page=" . ABJ404_PP . "&subpage=abj404_logs&id=" . $row['logsid'];
-            $trashlink = "?page=" . ABJ404_PP . "&subpage=" . $sub . "&id=" . $row['id'];
-            $ajaxTrashLink = "admin-ajax.php?action=trashLink&subpage=" . $sub . "&id=" . absint($row['id']);
-            $ignorelink = "?page=" . ABJ404_PP . "&subpage=" . $sub . "&id=" . $row['id'];
-            $laterlink = "?page=" . ABJ404_PP . "&subpage=" . $sub . "&id=" . $row['id'];
-            $deletelink = "?page=" . ABJ404_PP . "&subpage=" . $sub . "&remove=1&id=" . $row['id'];
+            $trashlink = "?page=" . ABJ404_PP . "&&subpage=abj404_captured&id=" . $row['id'];
+            $ajaxTrashLink = "admin-ajax.php?action=trashLink" . "&id=" . absint($row['id']);
+            $ignorelink = "?page=" . ABJ404_PP . "&&subpage=abj404_captured&id=" . $row['id'];
+            $laterlink = "?page=" . ABJ404_PP . "&&subpage=abj404_captured&id=" . $row['id'];
+            $deletelink = "?page=" . ABJ404_PP . "&subpage=abj404_captured&remove=1&id=" . $row['id'];
 
             if ($tableOptions['filter'] == ABJ404_TRASH_FILTER) {
                 $trashlink .= "&trash=0";
@@ -1244,7 +1244,7 @@ class ABJ_404_Solution_View {
             $editlink = "?page=" . ABJ404_PP . "&subpage=abj404_edit&id=" . absint($row['id']);
             $logslink = "?page=" . ABJ404_PP . "&subpage=abj404_logs&id=" . absint($row['logsid']);
             $trashlink = "?page=" . ABJ404_PP . "&id=" . absint($row['id']);
-            $ajaxTrashLink = "admin-ajax.php?action=trashLink&subpage=" . $sub . "&id=" . absint($row['id']);
+            $ajaxTrashLink = "admin-ajax.php?action=trashLink" . "&id=" . absint($row['id']);
             $deletelink = "?page=" . ABJ404_PP . "&remove=1&id=" . absint($row['id']);
 
             if ($tableOptions['filter'] == ABJ404_TRASH_FILTER) {

includes/ajax/trash_link_ajax.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function($) {
         // preventDefault() means don't move to the top of the page. 
         e.preventDefault();
         
+        var subpage = getURLParameter('subpage');
         var trashFilter = getURLParameter('filter');
         
         var row = $(this).closest("tr");
@@ -16,7 +17,8 @@ jQuery(document).ready(function($) {
             type : 'GET',
             dataType: "json",
             data: {
-                filter: trashFilter
+                filter: trashFilter,
+                subpage: subpage
             },
             success: function (data) {
                 if (data.result.startsWith("fail")) {

commit 21ca2b2e902bcd41655574f6da5fa212ff0122a0

Fix a home directory issue introduced in commit 2366c317c81a749e8918edb1566a59a9946ff345. Increase the 'tested up to' number to 5.2.

includes/objs/UserRequest.php
@@ -43,6 +43,7 @@ class ABJ_404_Solution_UserRequest implements JsonSerializable {
         
         $abj404logging = ABJ_404_Solution_Logging::getInstance();
         $f = ABJ_404_Solution_Functions::getInstance();
+        $abj404logic = new ABJ_404_Solution_PluginLogic();
         
         // hanlde the case where '///?gf_page=upload' is returned as the request URI.
         $urlToParse = urldecode($_SERVER['REQUEST_URI']);
@@ -51,7 +52,8 @@ class ABJ_404_Solution_UserRequest implements JsonSerializable {
         if (($containsHost === false) || ($containsHost >= 7) || (!is_array(parse_url(esc_url($urlToParse))))) {
             // we have something like //login.php and it needs to be http://host.com/login.php
             $urlToParse = str_replace('//', '/', $urlToParse);
-            $urlToParse = get_site_url() . $urlToParse;
+            $urlToParse = ltrim($abj404logic->removeHomeDirectory($urlToParse), '/');
+            $urlToParse = get_site_url() . '/' . $urlToParse;
         }
         
         $urlParts = parse_url(esc_url($urlToParse));