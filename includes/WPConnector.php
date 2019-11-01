6e420d66035cfe39a0c45f29d7077427db3f1977
Aaron <aaron@ajexperience.com>
2019-11-01 07:25:31 +0100

    Correct some requested URL issues.

--- includes/WPConnector.php
+++ includes/WPConnector.php
@@ -339,7 +339,7 @@ class ABJ_404_Solution_WordPress_Connector {
         if (( $redirect['status'] != ABJ404_STATUS_MANUAL && $redirect['status'] != ABJ404_STATUS_AUTO ) || $redirect['disabled'] != 0) {
             // It's a redirect that has been deleted, ignored, or captured.
             $abj404logging->errorMessage("processRedirect() was called with bad redirect data. Data: " .
-                    wp_kses_post(json_encode($redirect)));
+                    wp_kses_post(print_r($redirect, true)));
         }
 
         if ($redirect['type'] == ABJ404_TYPE_EXTERNAL) {
@@ -359,7 +359,7 @@ class ABJ_404_Solution_WordPress_Connector {
             $key = $redirect['final_dest'] . "|" . ABJ404_TYPE_HOME;
         } else {
             $abj404logging->errorMessage("Unrecognized redirect type in Connector: " .
-                    wp_kses_post(json_encode($redirect)));
+            	wp_kses_post(print_r($redirect, true)));
         }
 
         if ($key != "") {
