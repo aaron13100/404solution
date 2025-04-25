46c4893f904f7ada57538f1de834bfd5cbb3e568
Aaron <aaron13100@users.noreply.github.com>
2025-04-25 16:45:04 +0200

    * FIX: Avoid a logging issue while logging a DB error for leehodson.

--- includes/php/wordpress/WPDBExtension.php
+++ includes/php/wordpress/WPDBExtension.php
@@ -16,7 +16,11 @@ if (version_compare(PHP_VERSION, '7.0.0', '>=')) {
     class ABJ_404_Solution_WPDBExtension_PHP5 extends wpdb {
         public function public_strip_invalid_text_from_query($query) {
             try {
-                return $this->strip_invalid_text_from_query($query);
+                $result = $this->strip_invalid_text_from_query($query);
+                if (is_wp_error($result)) {
+                    return 'WP_Error: ' . $result->get_error_message();
+                }
+                return $result;
             } catch (Exception $e) {
                 return null;
             }
