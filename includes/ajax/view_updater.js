ecfab4dbd4351b078f58c1ff67d2239b77d9a4d5
Aaron <aaron13100@users.noreply.github.com>
2026-04-22 17:49:32 +0200

    fix(admin): remove duplicate bottom table separator after ajax hydration

--- includes/ajax/view_updater.js
+++ includes/ajax/view_updater.js
@@ -831,9 +831,13 @@ function paginationLinksChange(triggerItem, options) {
             // replace the tables - support both old (.wp-list-table) and new (.abj404-table) table classes
             var pageLinks = jQuery('.abj404-pagination-right');
             if (pageLinks.length > 0) {
-                jQuery(pageLinks[0]).replaceWith(result.paginationLinksTop);
+                var $topPagination = jQuery(result.paginationLinksTop);
+                $topPagination.addClass('abj404-pagination-top');
+                jQuery(pageLinks[0]).replaceWith($topPagination);
                 if (pageLinks.length > 1) {
-                    jQuery(pageLinks[1]).replaceWith(result.paginationLinksBottom);
+                    var $bottomPagination = jQuery(result.paginationLinksBottom);
+                    $bottomPagination.addClass('abj404-pagination-bottom');
+                    jQuery(pageLinks[1]).replaceWith($bottomPagination);
                 }
             }
             // Replace the table - try both class names
@@ -841,6 +845,36 @@ function paginationLinksChange(triggerItem, options) {
                 jQuery('.wp-list-table').replaceWith(result.table);
             } else if (jQuery('.abj404-table').length > 0) {
                 jQuery('.abj404-table').replaceWith(result.table);
+            }
+            // Update tab counts from AJAX response
+            if (result.tabCounts) {
+                jQuery('.abj404-content-tab[data-tab-filter]').each(function() {
+                    var filterVal = jQuery(this).attr('data-tab-filter');
+                    if (filterVal in result.tabCounts) {
+                        jQuery(this).find('.abj404-tab-count').text(result.tabCounts[filterVal]);
+                    }
+                });
+                jQuery('.abj404-content-tabs').removeAttr('data-tab-counts-placeholder');
+            }
+            // Update health bar from AJAX response (redirects page only)
+            if (typeof result.highImpactCapturedCount !== 'undefined' && result.statusCounts) {
+                var $bar = jQuery('.abj404-health-bar[data-health-bar-placeholder]');
+                if ($bar.length > 0) {
+                    var active = (result.statusCounts.all || 0) - (result.statusCounts.trash || 0);
+                    var high = result.highImpactCapturedCount || 0;
+                    var html;
+                    if (high === 0) {
+                        html = '<span class="abj404-health-dot abj404-health-green"></span>' +
+                            jQuery('<span>').text(active + ' redirects active, no URLs need attention').html();
+                    } else {
+                        html = '<span class="abj404-health-dot abj404-health-yellow"></span>' +
+                            jQuery('<span>').text(active + ' redirects active \u2014 ' + high + ' captured URLs have repeat visitors').html() +
+                            ' <a href="?page=' + (getURLParameter('page') || 'abj404_solution') + '&subpage=abj404_captured&filter=' +
+                            (result.statusCounts._capturedFilter || '') + '">View</a>';
+                    }
+                    $bar.html(html);
+                    $bar.removeAttr('data-health-bar-placeholder');
+                }
             }
             // Reinitialize table interactions (checkboxes, bulk actions) after AJAX refresh
             if (typeof window.abj404InitTableInteractions === 'function') {
